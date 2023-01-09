86d0eed3940cf3916a6ee02f61530b7ed13d0049
TMK04 <floer.learner.01@gmail.com>
2023-01-09 10:57:08 +0800

    refactor: rerun + resave + edit deployment
    It was the model directory. It had to be nested in the form of <name>/<version>

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -4,25 +4,27 @@ import tarfile
 
 bucket = "lia-model"
 endpoint_name = "lia-model-endpoint"
+saved_model = "model"
+key = f"{saved_model}.tar.gz"
 role = "arn:aws:iam::309100493331:role/LIA_SageMaker_Role"
 
 # Create a tarball of the saved model
-with tarfile.open("model.tar.gz", mode="w:gz") as f:
-  f.add("model", arcname="model")
+with tarfile.open(key, mode="w:gz") as f:
+  f.add(saved_model, arcname=saved_model)
 
 # Upload the tarball to S3
 s3 = client("s3")
-s3.upload_file("model.tar.gz", bucket, "model.tar.gz")
+s3.upload_file(key, bucket, key)
 
 # Create a SageMaker Model object
 model = TensorFlowModel(
   framework_version="2.8",
-  model_data=f"s3://{bucket}/model.tar.gz",
+  model_data=f"s3://{bucket}/{key}",
   role=role
 )
 
 model.deploy(
   endpoint_name=endpoint_name,
   initial_instance_count=1,
-  instance_type="ml.m5.large"
+  instance_type="ml.t2.medium"
 )
